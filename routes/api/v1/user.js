4da43ede4ad04f35f59294e6c53fb5e30e03087f
letha11 <ibkaanhar1@gmail.com>
2022-11-07 06:15:05 -0600

    implement error handling

--- routes/api/v1/user.js
+++ routes/api/v1/user.js
@@ -2,28 +2,102 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../../../config");
 
+/// firestore uses the same as gRPC status code
+/// https://github.com/grpc/grpc/blob/master/doc/statuscodes.md
+/// 5 = NOT_FOUND
+/// 6 = ALREADY_EXISTS
+
+const getErrorMessage = (statusCode) => {
+  let message;
+  switch (statusCode) {
+    case 5:
+      message = "User tidak dapat ditemukan";
+      break;
+    case 6:
+      message = "Data yang sama sudah ada di dalam database";
+      break;
+    default:
+      message = "Unknown Error";
+      break;
+  }
+
+  return message;
+};
+
 router
   .route("/")
   .post(async (req, res) => {
-    const data = req.body;
-    await Users.add(data);
-    res.send({ pesan: "Data berhasil ditambah!" });
+    try {
+      const data = req.body;
+      await Users.add(data);
+      res.status(201).send({
+        success: true,
+        message: "Data berhasil ditambah!",
+      });
+    } catch (e) {
+      console.error(e);
+      res.send({
+        success: false,
+        message: getErrorMessage(e?.code ?? 2),
+      });
+    }
   })
   .get(async (req, res) => {});
 
 router
   .route("/:id")
-  .get(async (req, res) => {})
   .put(async (req, res) => {
-    const ID = req.params.id;
-    const data = req.body;
-    await Users.doc(ID).update(data);
-    res.send({ pesan: "Data berhasil diubah!" });
+    try {
+      const ID = req.params.id;
+      const data = req.body;
+
+      /* 
+      No need to use this code because
+      firestore will throw an error if the user is not found.
+      
+      let user = await Users.doc(ID).get();
+      if (!user.exists) {
+        return res.status(404).send({
+          success: false,
+          message: getErrorMessage(5),
+        });
+
+      } */
+
+      await Users.doc(ID).update(data);
+      res.status(200).send({ success: true, message: "Data berhasil diubah!" });
+    } catch (e) {
+      console.error(e);
+      res.send({
+        success: false,
+        message: getErrorMessage(e?.code ?? 2),
+      });
+    }
   })
   .delete(async (req, res) => {
-    const ID = req.params.id;
-    await Users.doc(ID).delete();
-    res.send({ pesan: "Data berhasil dihapus!" });
+    try {
+      const ID = req.params.id;
+      let userRef = Users.doc(ID);
+      let user = await userRef.get();
+
+      // Check if user exists
+      if (!user.exists) {
+        return res.status(404).send({
+          success: false,
+          message: getErrorMessage(5),
+        });
+      }
+
+      await userRef.delete();
+
+      return res.status(204).send();
+    } catch (e) {
+      console.error(e);
+      res.send({
+        success: false,
+        message: getErrorMessage(e?.code ?? 2),
+      });
+    }
   });
 
 module.exports = router;
